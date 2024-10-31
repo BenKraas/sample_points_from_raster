7d2c6c58510b57a702391e857a9929d0ae264915
BenKraas <ben.kraas@gmail.com>
2024-10-31 17:43:52 +0100

    Final code

--- sample_vector_from_raster.py
+++ sample_vector_from_raster.py
@@ -5,56 +5,44 @@ from pathlib import Path
 
 # Third party imports
 import geopandas as gpd
-import pandas as pd
 import rasterio as rio
-import matplotlib.pyplot as plt
-from shapely.geometry import Point
 
-def sample_vector_from_raster(raster_path: str|Path, multipoint_path: str|Path, multipoint_primary_key: str) -> gpd.GeoDataFrame:
+
+def sample_vector_from_raster(raster_path: str|Path, multipoint_path: str|Path, new_column_name: str) -> gpd.GeoDataFrame:
 	"""
-	Sample raster values from a vector file.
+	Sample raster values from a multipoint file.
 	
 	Parameters:
 		raster_path (str|Path): Path to the raster file.
-		multipoint_path (str|Path): Path to the vector file.
-		vector_primary_key (str): The primary key of the vector file.
+		multipoint_path (str|Path): Path to the multipoint file.
+		new_column_name (str): Name of the new column to be created in the GeoDataFrame. 
+							   This should be descriptive of the raster values being sampled.
 	
 	Returns:
 		gpd.GeoDataFrame: A GeoDataFrame with the sampled values.
 	"""
-	# Check if the vector file has a primary key
-	if multipoint_primary_key not in vector.columns:
-		raise ValueError(f"Primary key {multipoint_primary_key} not found in the vector file.")
-	
-	# Read the vector file
-	vector = gpd.read_file(multipoint_path)
+	# read the multipoint file
+	gdf = gpd.read_file(multipoint_path)
+	gdf.dropna(subset=["geometry"], inplace=True) # optional: drop rows with missing geometries
 
-	points = [ # Placeholder
-		Point(625466, 5621289),
-		Point(626082, 5621627),
-		Point(627116, 5621680),
-		Point(625095, 5622358),
-	]
-	gdf = gpd.GeoDataFrame([1, 2, 3, 4], geometry=points, crs=32630)
-	coord_list = [(x, y) for x, y in zip(gdf["geometry"].x, gdf["geometry"].y)]
-
-	# Read the raster file
+	# read the raster file
 	src = rio.open(raster_path)
 
-	# Open the raster file
-	with rio.open(raster_path) as src:
-		# Initialize the dataframe to store the values. The primary key is the index. All raster bands are columns.
-		df = pd.DataFrame(index=vector[multipoint_primary_key])
+	# get the coordinates for each point in the multipoint file
+	coord_list = [(x, y) for x, y in zip(gdf["geometry"].x, gdf["geometry"].y)]
 
-		# Loop through the raster bands
-		for i in range(src.count):
-			# Sample the raster values and store the values in the dataframe
-			df[i] = [x[0] for x in src.sample(vector.geometry)]
+	# sample the raster values at the coordinates
+	gdf[new_column_name] = [x for x in src.sample(coord_list)]
 
-			df[i] = [x for x in src.sample(coord_list)]
+	return gdf
+	
 
-	# Merge the values with the vector file
-	# Will this change the file?
-	vector = vector.merge(df, left_on=multipoint_primary_key, right_index=True)
+if __name__ == "__main__":
+	raster_path = Path("./data/UTCI_pytherm_3m_v0.6.0_2024_305_15.tif")
+	multipoint_path = Path("./data/network_loc_20240405.geojson")
 
-	return vector
+	gdf = sample_vector_from_raster(raster_path, multipoint_path, "UTCI")
+
+	# save the GeoDataFrame to geojson
+	gdf.to_file("./data/sample.geojson", driver="GeoJSON")
+	
